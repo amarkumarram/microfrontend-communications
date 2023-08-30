0002a5b9a94599b39c97bd4d845845239ad23f96
Amar Kumar Ram <ram.amarkumar@gmail.com>
2023-08-30 13:11:23 +0530

    initial changes

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent {
+
+  public counter = 0;
+
+  handleCounter() {
+    this.counter += 1;
+  }
+}
