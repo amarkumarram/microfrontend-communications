ed233d876fd814d1e509f6c9633046469266ec20
Amar Kumar Ram <ram.amarkumar@gmail.com>
2023-12-18 07:56:23 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CounterRoutingModule } from './counter-routing.module';
 import { CounterComponent } from './counter.component';
+import { createCustomElement } from '@angular/elements';
+import "zone.js";
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
@@ -11,7 +14,15 @@ import { CounterComponent } from './counter.component';
   ],
   imports: [
     CommonModule,
-    CounterRoutingModule
+    CounterRoutingModule,
+    BrowserModule
   ]
 })
-export class CounterModule { }
+export class CounterModule { 
+  constructor(private injector: Injector){}
+
+  ngDoBootstrap() {
+    const counterApp = createCustomElement(CounterComponent, {injector: this.injector});
+    customElements.define('counter-app', counterApp);
+  }
+}
